161e4cac0486c767d57c73d1a3ab4609c4d653ae
Prawito <prawito.id@gmail.com>
2022-04-11 12:04:28 +0700

    project-quran-id

--- src/app/App.js
+++ src/app/App.js
@@ -1,24 +1,17 @@
-// import { BrowserRouter, Route } from "react-router-dom";
-// import { ThemeProvider } from "styled-components";
-// import { useContext } from "react";
-// import GlobalStyles from "../config/GlobalStyles";
-// import ThemeContext from "../config/ThemeContext";
-// import RouteQuran from "../config/RouteQuran";
-
+import { BrowserRouter, Route } from "react-router-dom";
+import React from "react";
+import RouteQuran from "../config/RouteQuran";
 import "./app.scss";
 
 function App() {
     // const { theme } = useContext(ThemeContext);
     return (
-        <h1>Bisa react</h1>
-        // <BrowserRouter basename="quran-id">
-        //     <Route>
-        //         {/* <ThemeProvider theme={{ theme }}> */}
-        //         {/* <GlobalStyles /> */}
-        //         <RouteQuran />
-        //         {/* </ThemeProvider> */}
-        //     </Route>
-        // </BrowserRouter>
+        // <h1>Bisa react</h1>
+        <BrowserRouter basename="quran-id">
+            <Route>
+                <RouteQuran />
+            </Route>
+        </BrowserRouter>
     );
 }
 
