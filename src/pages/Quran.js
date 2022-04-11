be2166a34220b27d08427dd302f772780c0d37f3
Prawito <prawito.id@gmail.com>
2022-04-11 11:44:59 +0700

    project-quran-id

--- src/pages/Quran.js
+++ src/pages/Quran.js
@@ -5,7 +5,7 @@ const Quran = () => {
         <div>
             <h1>
                 React Redux, React Loading, Styled Components, React Icons,
-                sass, React Router Dom, Github Pages, Coba
+                sass, React Router Dom, Github Pages, Coba, Bisa
             </h1>
         </div>
     );
